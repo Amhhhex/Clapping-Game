56e3ddea4d5a779f8c5ff26885b19814d107387b
hodginai <hodginai@OA-S138-25>
2026-03-03 17:15:01 -0500

    Clapping now works with an absolute value to state change for the check. The score is calculated based on the power of each hand and the distance between the hands. Also commented the code

--- Assets/Scripts/Arm Movement.cs
+++ Assets/Scripts/Arm Movement.cs
@@ -4,38 +4,46 @@ using Unity.Mathematics;
 
 public class ArmMovement : MonoBehaviour
 {
-
+    //The left and right hand game objects
     public GameObject leftHand;
-
     public GameObject rightHand;
 
-
+    //The speed at which the hands move in the Y-Axis
     public float handSpeed;
 
+    //The animation curves for moving in and returning back out
+    public AnimationCurve aniCurveMove;
+    public AnimationCurve aniCurveReturn;
+
+    //State switching between moving the left hand and returning the left hand
     public bool moveLeftHand;
     public bool returnLeftHand;
-    public AnimationCurve aniCurveMove;
-
-    public AnimationCurve aniCurveReturn;
+    
+    //The timers for interprolating the hand to the center and to the return position
     public float leftHandMoveTimer;
     public float leftHandReturnTimer;
     Vector3 leftReturnPosition;
 
-
+    //Same as above except for the right hand
     public bool moveRightHand;
     public bool returnRightHand;
     public float rightHandMoveTimer;
     public float rightHandReturnTimer;
     Vector3 rightReturnPosition;
 
+    //A global variable for distance and score measurement
     float handDistance;
     float score;
 
+    //seperate variables for left and right hand powers
     public float rightHandPower;
     public float leftHandPower;
 
+    //the amount in which the hand powers will increase
     public float powerIncrease;
 
+    //State change for checking claps
+    bool clapCheck;
 
 
 
@@ -48,7 +56,7 @@ public class ArmMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //Store the position of the left and right hands in local space
         Vector3 leftHandPosition = leftHand.transform.localPosition;
 
         Vector3 rightHandPosition = rightHand.transform.localPosition;
@@ -56,7 +64,7 @@ public class ArmMovement : MonoBehaviour
 
         
 
-
+        //Move the left and right hand's up or down based on which keys are held
         if(Keyboard.current.kKey.isPressed && !moveLeftHand && !returnLeftHand)
         {
 
@@ -83,6 +91,7 @@ public class ArmMovement : MonoBehaviour
         }
 
 
+        //While the left button is held down increase the leftHandPower var
         if(Mouse.current.leftButton.isPressed && !moveLeftHand)
         {
             leftHandPower += powerIncrease * Time.deltaTime;
@@ -93,7 +102,7 @@ public class ArmMovement : MonoBehaviour
             }
         }
          
-
+        //Once the left button is realeased start movement towards the center, disable movement of the hands while in movtion
         if(Mouse.current.leftButton.wasReleasedThisFrame && !moveLeftHand)
         {
             moveLeftHand = true;
@@ -102,16 +111,18 @@ public class ArmMovement : MonoBehaviour
             leftReturnPosition = leftHand.transform.localPosition;
         }
 
-       
+       //Interprolating the movement of the left hand towards the center of the screen
         if(moveLeftHand)
         {
             leftHandMoveTimer += Time.deltaTime;
 
             float aniCurveTimer = aniCurveMove.Evaluate(leftHandMoveTimer);
 
-            leftHandPosition = Vector3.Lerp(leftHandPosition, new Vector3(-1.3f, leftHandPosition.y, leftHandPosition.z), aniCurveTimer);
+            leftHandPosition = Vector3.Lerp(leftHandPosition, new Vector3(-1.19f, leftHandPosition.y, leftHandPosition.z), aniCurveTimer);
         }
 
+        //Once the hand reaches the middle of the screen, toggle the move to false and the return state to true
+        //also reset the timer
         if (leftHandMoveTimer >= 1f)
         {
 
@@ -121,6 +132,7 @@ public class ArmMovement : MonoBehaviour
             leftHandMoveTimer = 0f;
         }
 
+        //Interp the left hand from the center to its return position
         if(returnLeftHand)
         {
             leftHandReturnTimer += Time.deltaTime;
@@ -131,6 +143,7 @@ public class ArmMovement : MonoBehaviour
 
         }
 
+        //if the left hand is in the return position, set the timer and power to zero and give control back
         if(leftHandPosition == leftReturnPosition && returnLeftHand)
         {
             returnLeftHand = false;
@@ -139,6 +152,7 @@ public class ArmMovement : MonoBehaviour
         }
 
         //Right hand movement
+        //Everything that applied to left hand movement is the exact same here, instead just for the right hand
 
         if(Mouse.current.rightButton.isPressed && !moveRightHand)
         {
@@ -165,23 +179,26 @@ public class ArmMovement : MonoBehaviour
 
             float aniCurveTimer = aniCurveMove.Evaluate(rightHandMoveTimer);
 
-            rightHandPosition = Vector3.Lerp(rightHandPosition, new Vector3(-1.2f, rightHandPosition.y, rightHandPosition.z), aniCurveTimer);
+            rightHandPosition = Vector3.Lerp(rightHandPosition, new Vector3(-1.23f, rightHandPosition.y, rightHandPosition.z), aniCurveTimer);
         }
+
+        
+
+
+
+        if (handDistance <= 0.5f)
+        {
+            print(handDistance);
+            
+        }
+
+
 
         if (rightHandMoveTimer >= 1f)
         {
 
             returnRightHand = true;
             moveRightHand = false;
-
-            handDistance = Vector3.Distance(rightHandPosition, leftHandPosition);
-
-
-            score = math.remap(0, 1, 50, 0, handDistance);
-
-            score += leftHandPower;
-            score += rightHandPower;
-
 
             rightHandMoveTimer = 0f;
         }
@@ -207,17 +224,79 @@ public class ArmMovement : MonoBehaviour
 
 
 
+
+
+
+
+        //print("Absolute Value: " + math.abs(rightHandPosition.x - leftHandPosition.x));
+
+
+        //Once both hands are in the center of the screen, check if a clap was done
+        if (math.abs(rightHandPosition.x - leftHandPosition.x) < 1f)
+        {
+            print("CLAP!!!!!!!!!!");
+            clapCheck = true;
+        }
+
+        else
+        {
+            clapCheck = false;
+        }
+
+        //during a clap check, grab the Vector3 distance between the two hands
+        //remap that value to 50 - 0, 50 being the closest they could possibly be
+
+        //Then add the powers of the other two hands to the score
+        if (clapCheck)
+        {
+            handDistance = Vector3.Distance(rightHandPosition, leftHandPosition);
+
+
+            score = math.remap(0, 1, 50, 0, handDistance);
+
+            score += leftHandPower;
+            score += rightHandPower;
+
+        }
+
+        /*
+        print("Distance between the hands: " + handDistance);
+
         print(score);
 
+        */
+
+        
+        //Assigning a value with the various score totals
+        if (score >= 90f)
+        {
+            print("PERFECT!!");
+        }
+        else if (score >= 80f)
+        {
+            print("Great!");
+        }
+
+        else if(score >= 50f)
+        {
+            print("CMON MAN");
+        }
+
+        else if (score <= 50f)
+        {
+            print("pathetic");
+        }
+
+        
+
+        //restrict the left and right hand Y positions
+        leftHandPosition.y = Mathf.Clamp(leftHandPosition.y, -0.5f, 0.6f);
+
+        rightHandPosition.y = Mathf.Clamp(rightHandPosition.y, -0.5f, 0.6f);
 
 
 
-
-
-        leftHandPosition.y = Mathf.Clamp(leftHandPosition.y, -0.5f, 0.222f);
-
-        rightHandPosition.y = Mathf.Clamp(rightHandPosition.y, -0.5f, 0.222f);
-
+        //Change the transforms of the left and right hand to their newly update spots
         leftHand.transform.localPosition = leftHandPosition;
         rightHand.transform.localPosition = rightHandPosition;
 
