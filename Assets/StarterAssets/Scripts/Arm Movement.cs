319814dde26c259d33a3ec3106bc9955756d2f48
hodginai <hodginai@OA-S138-25>
2026-03-03 11:03:17 -0500

    Left and right hands now move up or down according to key presses

--- Assets/StarterAssets/Scripts/Arm Movement.cs
+++ Assets/StarterAssets/Scripts/Arm Movement.cs
@@ -1,7 +1,17 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ArmMovement : MonoBehaviour
 {
+
+    public GameObject leftHand;
+
+    public GameObject rightHand;
+
+
+    public float handSpeed;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +21,57 @@ public class ArmMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        Vector3 leftHandPosition = leftHand.transform.localPosition;
+
+        Vector3 rightHandPosition = rightHand.transform.localPosition;
+
+
+
+
+        if(Keyboard.current.kKey.isPressed)
+        {
+
+            leftHandPosition.y += handSpeed * Time.deltaTime;
+
+
+        }
+
+        if(Keyboard.current.jKey.isPressed)
+        {
+
+            leftHandPosition.y -= handSpeed * Time.deltaTime;
+
+        }
+
+        if(Keyboard.current.mKey.isPressed)
+        {
+            rightHandPosition.y += handSpeed * Time.deltaTime;
+        }
+
+        if(Keyboard.current.nKey.isPressed)
+        {
+            rightHandPosition.y -= handSpeed * Time.deltaTime;
+        }
+
+
+
+       
+
+
+        leftHandPosition.y = Mathf.Clamp(leftHandPosition.y, -0.5f, 0.222f);
+
+        rightHandPosition.y = Mathf.Clamp(rightHandPosition.y, -0.5f, 0.222f);
+
+        print(leftHandPosition.y);
+
+        print("hello");
+
+        leftHand.transform.localPosition = leftHandPosition;
+        rightHand.transform.localPosition = rightHandPosition;
+
+
+
         
     }
 }
