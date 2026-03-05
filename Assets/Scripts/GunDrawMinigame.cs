6d0253214f730618da34d381d0ba419c748adebc
Mont <mont.lettered@gmail.com>
2026-03-05 00:55:00 -0500

    Playable WIP gun showdown minigame
    Implemented a minigame where you clap a pistol to fire in a western style showdown. TMP Essentials also imported

--- Assets/Scripts/GunDrawMinigame.cs
+++ Assets/Scripts/GunDrawMinigame.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 
 public class GunDrawMinigame : MonoBehaviour
 {
@@ -6,11 +7,14 @@ public class GunDrawMinigame : MonoBehaviour
     public HandCollision rightHandCollidedScr;
     public GameObject playerObj;
     public GameObject gunObj;
+    public TextMeshProUGUI drawAnnounceText;
     ArmMovement armMovementScr;
     CharacterMovement characterMovementScr;
     float currentTimeInDraw;
     float currentTimeInShowdown;
     float totalDrawTime;
+    float totalShowdownTime = 2f;
+    bool showdownStarted;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,6 +29,39 @@ public class GunDrawMinigame : MonoBehaviour
         //If the arm leaves the gun, the collision is reset
         if (armMovementScr.returnLeftHand) { leftHandCollidedScr.handCollidedObj = null; }
         if (armMovementScr.returnRightHand) { rightHandCollidedScr.handCollidedObj = null; }
+
+        if (showdownStarted)
+        {
+            currentTimeInDraw += Time.deltaTime;
+            if (currentTimeInDraw >= totalDrawTime)
+            {
+                drawAnnounceText.text = "DRAW!";
+                drawAnnounceText.gameObject.SetActive(true);
+                currentTimeInShowdown += Time.deltaTime;
+
+                if (leftHandCollidedScr.handCollidedObj == gunObj && leftHandCollidedScr.handCollidedObj == gunObj)
+                {
+                    drawAnnounceText.text = "You win";
+                    //Play gunshot
+                    //Spin Gunslinger
+                    //Assume new position
+                    currentTimeInDraw = 0;
+                    currentTimeInShowdown = 0;
+                    characterMovementScr.enabled = true;
+                    showdownStarted = false;
+                }
+
+                if (currentTimeInShowdown >= totalShowdownTime)
+                {
+                    //Play gunshot
+                    drawAnnounceText.text = "DIE!";
+                    currentTimeInDraw = 0;
+                    currentTimeInShowdown = 0;
+                    characterMovementScr.enabled = true;
+                    showdownStarted = false;
+                }
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision playerCollider)
@@ -33,28 +70,8 @@ public class GunDrawMinigame : MonoBehaviour
         playerObj.transform.localPosition = new Vector3(gunObj.transform.position.x, playerObj.transform.position.y, gunObj.transform.position.z - 1);
         Camera.main.transform.localRotation = Quaternion.identity;
         characterMovementScr.enabled = false;
-
         //Time draw is random between 5-15 seconds
         totalDrawTime = Random.Range(5, 15);
-        currentTimeInDraw += Time.deltaTime;
-
-        if (currentTimeInDraw >= totalDrawTime) 
-        {
-            currentTimeInShowdown += Time.deltaTime;
-            if ( currentTimeInShowdown >= totalDrawTime)
-            {
-                //Play gunshot
-                Debug.Log("You lose");
-            }
-        }
-
-        if (leftHandCollidedScr.handCollidedObj == gunObj && leftHandCollidedScr.handCollidedObj == gunObj)
-        {
-            Debug.Log("You win");
-            //Play gunshot
-            //Spin Gunslinger
-            //Assume new position
-            characterMovementScr.enabled = true;
-        }
+        showdownStarted = true;
     }
 }
